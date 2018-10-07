5a6de2fa3fa5ad091badb704c4be9cb19b48a522
Boris Arturo González Rivera <borisgr04@gmail.com>
2018-10-06 23:06:58 -0500

    Agregar archivos de proyecto.

---
+++ ExampleJsReportServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleJsReportServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
